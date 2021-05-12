b58001d2bdb193b13cd5439a06c0e69b080aa856
marchiello-dev <marcelo.jara.vasquez1@gmail.com>
2021-05-11 22:34:16 -0400

    muchos commit

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -46,6 +46,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // wena los cabros si esta shit la ven es pq funco
         //commit junior
 
+
+        //asdasdasd
+
+        //dsfsdfsdfds
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
